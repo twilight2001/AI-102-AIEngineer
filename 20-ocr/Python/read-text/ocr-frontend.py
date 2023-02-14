0a53887a22ebfe4f53d4872eb6f76707a15ea274
twilight2001 <twilight2001@users.noreply.github.com>
2023-02-14 18:43:09 +0800

    Add files via upload

--- 20-ocr/Python/read-text/ocr-frontend.py
+++ 20-ocr/Python/read-text/ocr-frontend.py
@@ -1,20 +1,13 @@
-from dotenv import load_dotenv 
-import time
-from PIL import Image, ImageDraw 
-from glob import glob
 import os
-from os import path  
-import sys 
-import streamlit as st 
-import json
-import requests 
-
+import requests
+from dotenv import load_dotenv
+import streamlit as st
 # Import namespaces
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from msrest.authentication import CognitiveServicesCredentials
 
-# Get Configuration Settings
+
 load_dotenv()
 cog_endpoint = os.getenv('COG_SERVICE_ENDPOINT')
 cog_key = os.getenv('COG_SERVICE_KEY')
@@ -23,99 +16,50 @@ cog_key = os.getenv('COG_SERVICE_KEY')
 credential = CognitiveServicesCredentials(cog_key)
 cv_client = ComputerVisionClient(cog_endpoint, credential)
 
+ocr_url = cog_endpoint + "vision/v3.0/ocr"
 
-# function to read text from an image file
-def ocr_image(file, endpoint, subscription_key):
-    image = Image.open(file)
-    image_data = image.tobytes()
+# Define namespace for Streamlit app
+namespace = st.sidebar.text_input("Enter a namespace for your analysis")
 
-    response = requests.post(
-        url=endpoint + "/vision/v3.1-preview.3/read/analyze",
-        headers={
-            "Ocp-Apim-Subscription-Key": subscription_key,
-            "Content-Type": "application/octet-stream"
-        },
-        data=image_data
-    )
+# Define global configuration for the Streamlit app
+st.set_page_config(page_title="OCR App", page_icon=":pencil2:", layout="wide")
 
-    operation_url = response.headers["Operation-Location"]
-    operation_id = operation_url.split("/")[-1]
+def process_image(image_path):
+    # Set the headers for the REST API call.
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
+    params = {'language': 'unk', 'detectOrientation': 'true'}
+    data = open(image_path, 'rb').read()
 
-    # check the read operation status
-    while True:
-        response = requests.get(
-            url=operation_url,
-            headers={"Ocp-Apim-Subscription-Key": subscription_key}
-        )
-        status = response.json()["status"]
-        if status == "succeeded":
-            break
-        elif status == "failed":
-            st.error("OCR operation failed")
-            return
+    # Call the REST API.
+    response = requests.post(ocr_url, headers=headers, params=params, data=data)
 
-    # extract the text from the result
-    result = response.json()
-    text = ""
-    for line in result["analyzeResult"]["readResults"][0]["lines"]:
-        text += line["text"] + " "
-    return text
+    # Get the JSON response.
+    response_json = response.json()
 
-# function to read text from a PDF file
-def ocr_pdf(file, endpoint, subscription_key):
-    with open(file, 'rb') as pdf_file:
-        pdf_bytes = pdf_file.read()
+    # Extract the text from the JSON response.
+    extracted_text = ''
+    for region in response_json['regions']:
+        for line in region['lines']:
+            for word in line['words']:
+                extracted_text += ' ' + word['text']
 
-    response = requests.post(
-        url=endpoint + "/vision/v3.1-preview.3/read/analyze",
-        headers={
-            "Ocp-Apim-Subscription-Key": subscription_key,
-            "Content-Type": "application/pdf"
-        },
-        data=pdf_bytes
-    )
+    return extracted_text
 
-    operation_url = response.headers["Operation-Location"]
-    operation_id = operation_url.split("/")[-1]
+# Define the Streamlit app
+def app():
+    st.title("OCR App")
+    st.write("Upload an image or PDF to extract text")
 
-    # check the read operation status
-    while True:
-        response = requests.get(
-            url=operation_url,
-            headers={"Ocp-Apim-Subscription-Key": subscription_key}
-        )
-        status = response.json()["status"]
-        if status == "succeeded":
-            break
-        elif status == "failed":
-            st.error("OCR operation failed")
-            return
+    file = st.file_uploader("Choose a file", type=["jpg", "pdf"])
+    if file is not None:
+        with open(f"{namespace}.pdf", "wb") as f:
+            f.write(file.getbuffer())
+        st.write("File uploaded successfully!")
 
-    # extract the text from the result
-    result = response.json()
-    text = ""
-    for page in result["analyzeResult"]["readResults"]:
-        for line in page["lines"]:
-            text += line["text"] + " "
-    return text
+        extracted_text = process_image(f"{namespace}.pdf")
 
-# create a file uploader for the user to select an image or PDF
-file = st.file_uploader("Upload file", type=["jpg", "jpeg", "png", "pdf"])
+        st.write("Extracted Text:")
+        st.write(extracted_text)
 
-# if a file has been uploaded
-if file is not None:
-    # if the file is an image
-    if file.type.split("/")[0] == "image":
-        # read the text from the image
-        text = ocr_image(file, endpoint, subscription_key)
-    # if the file is a PDF
-    elif file.type == "application/pdf":
-        # read the text from the PDF
-        text = ocr_pdf(file, endpoint, subscription_key)
-    # if the file is of an unsupported type
-    else:
-        st.error("Unsupported file type")
-        text = ""
-
-    # display the text
-    st.write(text)
+# Run the Streamlit app
+app()
