466b4094259373ee5681c98101e1a26775168ac8
twilight2001 <twilight2001@users.noreply.github.com>
2023-02-14 20:44:20 +0800

    Add files via upload

--- 20-ocr/Python/read-text/read-real.py
+++ 20-ocr/Python/read-text/read-real.py
@@ -27,25 +27,21 @@ def main():
         cv_client = ComputerVisionClient(cog_endpoint, credential)
 
         # Menu for text reading functions
-        print('1: Use Read API for image\n2: Use Read API for document\n3: Read handwriting\nAny other key to quit')
+        print('1: ACS Read API for handwriting1\n2: ACS Read API for pdf\nAny other key to quit')
         command = input('Enter a number:')
         if command == '1':
-            image_file = os.path.join('.\images','Lincoln.jpg')
+            image_file = os.path.join('.\images','essay1.jpg')
             GetTextRead(image_file)
         elif command =='2':
-            image_file = os.path.join('.\images','Rome.pdf')
-            GetTextRead(image_file)
-        elif command =='3':
-            image_file = os.path.join('.\images','Note.jpg')
-            GetTextRead(image_file)
+            image_file = os.path.join('.\images','530188632021PartA.pdf')
+            GetTextRead(image_file) 
                 
     except Exception as ex:
         print(ex)
-
-
+ 
 
 def GetTextRead(image_file):
-    print('Reading text in {}\n'.format(image_file))
+    print('Extracting text in {}\n'.format(image_file))
     # Use Read API to read text in image
     with open(image_file, mode="rb") as image_data:
         read_op = cv_client.read_in_stream(image_data, raw=True)
@@ -67,7 +63,7 @@ def GetTextRead(image_file):
             for line in page.lines:
                 print(line.text)
                 # Uncomment the following line if you'd like to see the bounding box 
-                print(line.bounding_box)
+                # print(line.bounding_box)
 
 if __name__ == "__main__":
     main()
