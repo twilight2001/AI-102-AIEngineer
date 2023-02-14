32c54391a0abdcee0b7d0ac4ceba6630a0a6a63e
twilight2001 <twilight2001@users.noreply.github.com>
2023-02-14 18:56:40 +0800

    Add files via upload
    fix streamlit to beginning

--- 20-ocr/Python/read-text/ocr-frontend.py
+++ 20-ocr/Python/read-text/ocr-frontend.py
@@ -7,6 +7,8 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from msrest.authentication import CognitiveServicesCredentials
 
+# Define global configuration for the Streamlit app
+st.set_page_config(page_title="OCR App", page_icon=":pencil2:", layout="wide")
 
 load_dotenv()
 cog_endpoint = os.getenv('COG_SERVICE_ENDPOINT')
@@ -21,12 +23,11 @@ ocr_url = cog_endpoint + "vision/v3.0/ocr"
 # Define namespace for Streamlit app
 namespace = st.sidebar.text_input("Enter a namespace for your analysis")
 
-# Define global configuration for the Streamlit app
-st.set_page_config(page_title="OCR App", page_icon=":pencil2:", layout="wide")
+
 
 def process_image(image_path):
     # Set the headers for the REST API call.
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
+    headers = {'Ocp-Apim-Subscription-Key': cog_key, 'Content-Type': 'application/octet-stream'}
     params = {'language': 'unk', 'detectOrientation': 'true'}
     data = open(image_path, 'rb').read()
 
@@ -47,7 +48,7 @@ def process_image(image_path):
 
 # Define the Streamlit app
 def app():
-    st.title("OCR App")
+    st.title("Azure OCR App")
     st.write("Upload an image or PDF to extract text")
 
     file = st.file_uploader("Choose a file", type=["jpg", "pdf"])
